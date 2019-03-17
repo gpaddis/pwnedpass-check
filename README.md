224f80010924deb8a7889a0352eb40a6e5e40fa3
Gianpiero Addis <gianpiero.addis@gmail.com>
2019-03-17 17:49:44 +0100

    Refactor code, extract checkErr

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Build the package and check for breached passwords:
 
-```sh
+```
 $ go build
 $ ./pwnedpass-check pass123
-The password pass123 was found in 82170 breaches.
+The password pass123 was pwned and appears 82170 times in the database.
 ```
--- pwnedpass-check.go
+++ pwnedpass-check.go
@@ -1,5 +1,8 @@
 package main
 
+// https://haveibeenpwned.com/API/v2#PwnedPasswords
+// GET https://api.pwnedpasswords.com/range/{first 5 SHA1 hash chars}
+
 import (
 	"crypto/sha1"
 	"encoding/hex"
@@ -10,9 +13,6 @@ import (
 	"strings"
 )
 
-// https://haveibeenpwned.com/API/v2#PwnedPasswords
-// GET https://api.pwnedpasswords.com/range/{first 5 SHA1 hash chars}
-
 func getSHA1Hash(password string) string {
 	hasher := sha1.New()
 	hasher.Write([]byte(password))
@@ -20,25 +20,30 @@ func getSHA1Hash(password string) string {
 	return strings.ToUpper(hash)
 }
 
-func main() {
-	pass := os.Args[1]
-	hash := getSHA1Hash(pass)
-	res, err := http.Get("https://api.pwnedpasswords.com/range/" + hash[0:5])
+func checkErr(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func main() {
+	password := os.Args[1]
+	hash := getSHA1Hash(password)
+	res, err := http.Get("https://api.pwnedpasswords.com/range/" + hash[0:5])
+	checkErr(err)
 	defer res.Body.Close()
 	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		panic(err)
-	}
+	checkErr(err)
 
-	result := strings.Fields(string(body))
+	hashList := strings.Fields(string(body))
 	suffix := hash[5:]
-	for _, r := range result {
-		if strings.Contains(r, suffix) {
-			count := strings.Split(r, ":")[1]
-			fmt.Println("The password", pass, "was found in", count, "breaches.")
+	for _, h := range hashList {
+		if strings.Contains(h, suffix) {
+			count := strings.Split(h, ":")[1]
+			fmt.Println("The password", password, "was pwned and appears", count, "times in the database.")
+			return
 		}
 	}
+
+	fmt.Println("The password", password, "was not found in any breaches.")
 }
